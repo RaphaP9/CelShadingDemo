f433474b5955f903826b60617ac75b79e65ed894
Raphael <115659111+RaphaP9@users.noreply.github.com>
2026-04-19 00:53:07 -0500

    ADD FinalParams

--- Assets/Shaders/RealtimeLightsCelShader/CellShadingFunctions.hlsl
+++ Assets/Shaders/RealtimeLightsCelShader/CellShadingFunctions.hlsl
@@ -22,7 +22,7 @@ float PlaceLightingInBand(float Lighting, float LightingBands)
     return floor(Lighting * LightingBands) / LightingBands;
 }
 
-float3 CalculateCelShading(Light l, SurfaceVariables s, float minimumLight, bool enlightenAllObjectWithMinimumLight)
+float3 CalculateCelShading(Light l, SurfaceVariables s, float minimumLight, float darkSideMinimumLightMuliplier, bool enlightenAllObjectWithMinimumLight)
 {
     float diffuse = saturate(dot(s.normal, l.direction));
     //float diffuse = dot(s.normal, l.direction) * 0.5 + 0.5; //Remaping to 0 - 1 //Makes Object enlighten more in zones that otherwise would not enlighten, Also produces artifacts
@@ -34,29 +34,31 @@ float3 CalculateCelShading(Light l, SurfaceVariables s, float minimumLight, bool
     
     bandedLighting = lerp(minimumLight, 1.0, bandedLighting); //Remapping
       
-    if (!enlightenAllObjectWithMinimumLight)
-    {
-        float mask = (diffuse > 0.0001) ? 1.0 : 0.0; //Define a mask to only enlighten parts where diffuse has a value over 0 (0.0001)
-        bandedLighting = lerp(0.0, bandedLighting, mask); //BandedLighting becomes 0 if diffuse was 0
-    }
-    
+    //Apply either darkSide Lighting or cut all lighting from darkSide
+    float darkSideLight = enlightenAllObjectWithMinimumLight ? minimumLight * darkSideMinimumLightMuliplier : 0.0; 
+    bandedLighting = diffuse > 0.0001 ? bandedLighting : darkSideLight;
+
+    //Highlight Calculations
     float highlight = diffuse >= s.highlightThreshold ? s.hightlightIntensity : 0.0;
     
+    //Specular Calculations
     float3 h = SafeNormalize(l.direction + s.view);
     float primitiveSpecular = saturate(dot(s.normal, h));
     float specular = primitiveSpecular >= s.specularThreshold ? s.specularIntensity : 0.0;
     specular = diffuse > 0.0001 ? specular : 0.0;
     
+    //Rim Calculations
     float primitiveRim = 1 - dot(s.view, s.normal);
     float rim = primitiveRim >= s.rimThreshold ? s.rimIntensity : 0.0;
     rim = diffuse > 0.0001 ? rim : 0.0;
     
-    bandedLighting = pow(bandedLighting, s.bandsPowerShift); //Power the lighting to get a nice effect
-    
     float addOn = max(highlight, specular);
     addOn = max(rim, addOn);
+    bandedLighting += addOn;
     
-    return l.color * (bandedLighting + addOn);
+    bandedLighting = pow(bandedLighting, s.bandsPowerShift); //Power the lighting to get a nice effect, enlighten bands if < 1, darken if > 1;
+    
+    return l.color * bandedLighting;
 }
 #endif
 
@@ -67,6 +69,7 @@ void LightingCelShaded_float(
     float LightingBands,
     float MinimumMainLight,
     float MinimumAdditionalLight,
+    float DarkSideMinimumLightMuliplier,
     float BandsPowerShift,
     float HightlightThreshold,
     float HightlightIntensity,
@@ -100,7 +103,7 @@ void LightingCelShaded_float(
     #endif
     
     Light light = GetMainLight(shadowCoord);
-    Color = CalculateCelShading(light, s, MinimumMainLight, true);
+    Color = CalculateCelShading(light, s, MinimumMainLight, DarkSideMinimumLightMuliplier, true);
     
     #if _ADDITIONAL_LIGHTS
     int pixelLightCount = GetAdditionalLightsCount();
@@ -108,9 +111,9 @@ void LightingCelShaded_float(
     for (int i = 0; i < pixelLightCount; i++)
     {
         light = GetAdditionalLight(i, Position, 1);
-        Color += CalculateCelShading(light, s, MinimumAdditionalLight, false);
+        Color += CalculateCelShading(light, s, MinimumAdditionalLight, DarkSideMinimumLightMuliplier, false);
     }
-    #endif
+#endif
     
 #endif
 }
