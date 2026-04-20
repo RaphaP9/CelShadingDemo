822b1629d77f11216b6c118a4d3ebda42c749dbd
Raphael <115659111+RaphaP9@users.noreply.github.com>
2026-04-20 10:13:45 -0500

    ADD HLSL Functions

--- Assets/Shaders/CelShader/CellShadingFunctions.hlsl
+++ Assets/Shaders/CelShader/CellShadingFunctions.hlsl
@@ -22,12 +22,36 @@ float PlaceLightingInBand(float Lighting, float LightingBands)
     return floor(Lighting * LightingBands) / LightingBands; //Use ceil if priority to high light levels is desired
 }
 
+float CalculateHighlight(float lighting, float threshold, float intensity)
+{
+    float highlight = step(threshold, lighting) * intensity;
+    return highlight;
+}
+
+float CalculateSpecular(float3 lightDirection, float3 viewDirection, float3 surfaceNormal, float lighting, float threshold, float intensity)
+{
+    float3 h = SafeNormalize(lightDirection + viewDirection);
+    float primitiveSpecular = saturate(dot(surfaceNormal, h)); //Primitive specular is a gradient
+    float specular = step(threshold, primitiveSpecular) * intensity; //Only Enlighten parts where the primitive specular is over the threshold
+    specular = step(0.0001, lighting) * specular; //Mask with diffuse (do not enlighten parts where diffuse = 0 - happens on shadowed parts - use 0.0001 due to step being greater or equal to)
+    return specular;
+}
+
+float CalculateRim(float3 viewDirection, float3 surfaceNormal, float lighting, float threshold, float intensity)
+{
+    float primitiveRim = 1 - dot(viewDirection, surfaceNormal); //Primitive rim is also a gradient
+    float rim = step(threshold, primitiveRim) * intensity; //Exact same logic as specular
+    rim = step(0.0001, lighting) * rim;
+    return rim;
+}
+
 float3 CalculateCelShading(Light l, SurfaceVariables s, float minimumLight, float darkSideMinimumLightMuliplier, bool enlightenDarkSideWithMinimumLight)
 {
     float diffuse = saturate(dot(s.normal, l.direction));
     float attenuation = l.distanceAttenuation * l.shadowAttenuation;
     attenuation = saturate(attenuation); 
-    //Important to saturate attenuation, otherwise it can increase the number of light bands when attenuation (specifically distance Attenuation) goes over 1 (light very close to the surface).
+    //Important to saturate attenuation before multiplying with diffuse
+    //Otherwise, if attenuation (specifically distance Attenuation) goes over 1 (surface very close to light source), diffuse * attenuation can go over 1, increasing the number of light bands
     //PlaceLightingInBand(..) expects a 0 - 1 lighting input
     
     diffuse *= attenuation;  
@@ -40,23 +64,13 @@ float3 CalculateCelShading(Light l, SurfaceVariables s, float minimumLight, floa
     float darkSideLight = enlightenDarkSideWithMinimumLight ? minimumLight * darkSideMinimumLightMuliplier : 0.0;
     bandedLighting = diffuse > 0.0 ? bandedLighting : darkSideLight;
 
-    //Highlight Calculations
-    float highlight = diffuse > s.highlightThreshold ? s.hightlightIntensity : 0.0;
+    //AddOn Calculations
+    float highlight = CalculateHighlight(diffuse, s.highlightThreshold, s.hightlightIntensity);
+    float specular = CalculateSpecular(l.direction, s.view, s.normal, diffuse, s.specularThreshold, s.specularIntensity);
+    float rim = CalculateRim(s.view, s.normal, diffuse, s.rimThreshold, s.rimIntensity);
     
-    //Specular Calculations
-    float3 h = SafeNormalize(l.direction + s.view);
-    float primitiveSpecular = saturate(dot(s.normal, h));
-    float specular = primitiveSpecular > s.specularThreshold ? s.specularIntensity : 0.0; //Only Enlighten parts where the primitive specular is over the threshold
-    specular = diffuse > 0.0 ? specular : 0.0; //Mask with diffuse (do not enlighten parts where diffuse = 0)
-    
-    //Rim Calculations
-    float primitiveRim = 1 - dot(s.view, s.normal);
-    float rim = primitiveRim > s.rimThreshold ? s.rimIntensity : 0.0; //Exact same logic as specular
-    rim = diffuse > 0.0 ? rim : 0.0;
-    
-    //Find the max value among the three AddOns(Highligh, Specular and Rim), as we dont want overlapping AddOns, only the more intense
-    float addOn = max(highlight, specular);
-    addOn = max(rim, addOn);
+    //Find the max value among the three AddOns(Highligh, Specular and Rim), as we dont want overlapping AddOns in each pixel, only the most intense one
+    float addOn = max(highlight, max(specular, rim));
     bandedLighting += addOn; //Add to the bandedLighting
     
     //Power the lighting to get a nice effect (If bandsPowerShift < 1, enlighten and uniformize lighting, if bandsPowerShift > 1, darken all light but stand out addOns)
